fcfe58ec2f2bb9a73ad3f58cba3d7b880b3c9980
Tomasz Pazurkiewicz <t.pazurkiewicz@gmail.com>
2011-08-22 16:03:58 +0200

    (signed_request) validate request time (close #905).
    Change-Id: I94c2324d66ddb9cc37b07638f0e9e426227b6885

--- facebook_signed_request/middleware.py
+++ facebook_signed_request/middleware.py
@@ -1,10 +1,16 @@
 import base64
+import time
 
 from django.http import QueryDict
 from django.utils import simplejson as json
 import logging
 
 logger = logging.getLogger(__name__)
+
+EXPIRATION_TIME = 5 * 60
+
+class SignedRequestException(Exception):
+    pass
 
 class SignedRequestMiddleware(object):
     def process_request(self, request):
@@ -13,12 +19,15 @@ class SignedRequestMiddleware(object):
                 raw_signature, payload = request.POST['signed_request'].split('.', 1)
                 signature = self.decode_data(raw_signature)
                 data = json.loads(self.decode_data(payload))
+                self.validate_time(data['issued_at'])
                 request.facebook = data
             except ValueError: # json loads & split
                 logger.error('"signed_request" is invalid json string')
                 pass
             except TypeError: # base64 decode
                 logger.error('Cannot decode "signed_request"')
+            except SignedRequestException as exception:
+                logger.error(exception.message)
             request.POST = QueryDict('')
             request.method = 'GET'
 
@@ -26,3 +35,7 @@ class SignedRequestMiddleware(object):
         padding = "=" * ((4 - len(data) % 4) % 4)
         return base64.urlsafe_b64decode(str(data) + padding)
 
+    def validate_time(self, timestamp):
+        now = time.time()
+        if timestamp + EXPIRATION_TIME < now:
+            raise SignedRequestException('"signed_request" expired')
