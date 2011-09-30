a738fb4878cc962968f9ced7e6c7af945120ca46
Tomasz Wysocki <tomasz@wysocki.info>
2011-09-30 16:38:43 +0200

    Release facebook-signed-request.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='facebook-signed-request',
-    version='0.1',
+    version='1.0',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
