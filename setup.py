a3cc886243f3ab4c81c7c4a3635277543baec30e
Jakub Skiepko <jakub@pozytywnie.pl>
2016-03-03 09:39:28 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='facebook-signed-request',
-    version='3.0.2',
+    version='3.0.3',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
